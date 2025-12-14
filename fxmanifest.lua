de1b55dd816667b84ec88c138e3d0c30f3e9acb7
Rubberduckscripts <mralleycatz@gmail.com>
2025-12-14 13:27:24 +0000

    Update fxmanifest.lua
    Updated

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,18 +2,19 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Rubber Duck Scripts (RDS)'
-description 'RDS Custom Crafting System'
+description 'RDS Custom Weapon Crafting'
 version '1.0.0'
 
 shared_scripts {
+    '@ox_lib/init.lua',
     'config.lua'
 }
 
 client_scripts {
-    'client/*.lua'
+    'client/main.lua'
 }
 
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
-    'server/*.lua'
+    'server/main.lua'
 }
